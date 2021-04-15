c076f65f41cc081da0f5e19fc1aa5f72e3e11c9d
aleksey1219 <82465257+aleksey1219@users.noreply.github.com>
2021-04-15 20:16:44 +0300

    Add files via upload
    задание для урока номер 2

---
+++ lesson2/Runner.java
@@ -0,0 +1,11 @@
+package ru.synergyitacademy.lesson2;
+
+public class Runner {
+
+    public static void main(String[] args) {
+     int x = 345 * 789;
+     int z = 12 * 6;
+     int y = z+x-(345-12)*(789-6);
+        System.out.println("x:"+ x + " z:" + z + " y:"+y );
+    }
+}
