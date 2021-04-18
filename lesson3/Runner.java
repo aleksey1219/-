b1fb6e010e8fc3376f0db0c361de64b3fb7bf34f
aleksey1219 <82465257+aleksey1219@users.noreply.github.com>
2021-04-18 14:30:02 +0300

    Add files via upload

---
+++ lesson3/Runner.java
@@ -0,0 +1,27 @@
+package ru.synergyitacademy.lesson3;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        Test phyria = new Test();
+        phyria.display();
+    }
+}
+
+class Test {
+    Scanner scanner = new Scanner(System.in);
+
+    public int F(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        } else {
+            return (F(n - 1)) + F(n - 2);
+        }
+    }
+
+    void display() {
+        System.out.print("введите  n-ый член последовательности Фибоначчи: ");
+        System.out.println(F(scanner.nextInt()));
+    }
+}
