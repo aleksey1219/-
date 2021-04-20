2aefa8ec30f3f9d04317abc9ebccc45d5d846479
aleksey1219 <82465257+aleksey1219@users.noreply.github.com>
2021-04-20 18:38:40 +0300

    Add files via upload

---
+++ company/mass.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class mass {
+    public static void main(String[] args) {
+        Matrix mA1 = new Matrix();
+        Matrix mA2 = new Matrix();
+
+        System.out.println("Размер матрицы А: " + mA1.getM() + " х " + mA1.getN());
+
+        System.out.println("Размер матрицы В: " + mA2.getM() + " х " + mA2.getN());
+        System.out.println("Сумма матриц А и В:");
+
+
+        }
+    }
+
