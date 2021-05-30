5da540052376656913242232d1f93078d79eb35c
aleksey1219 <82465257+aleksey1219@users.noreply.github.com>
2021-05-30 19:37:39 +0400

    Update Converter.java

--- lesson8/Converter.java
+++ lesson8/Converter.java
@@ -1,8 +1,9 @@
 package ru.synergyitacademy.lesson8;
 
 public interface Converter {
-    double convert (double perevod);
-static Converter getConverter (Temperature temperature){
-return temperature.getConverter();
-}
+    double convert(double perevod);
+
+    static Converter getConverter(Temperature temperature) {
+        return temperature.getConverter();
+    }
 }
